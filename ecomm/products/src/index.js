b38bcfd21f0bc7ad12cdacf7f95cc4ec00ca3798
Enio Gabriel <enio@nexti.com>
2023-03-25 19:56:00 -0300

    Async Script Loading

--- ecomm/products/src/index.js
+++ ecomm/products/src/index.js
@@ -1,11 +1,1 @@
-import faker from "faker";
-
-let products = '';
-
-for (let index = 0; index < 5; index++) {
-    const name = faker.commerce.product();
-
-    products += `<div>${name}</div>`
-}
-
-document.querySelector("#dev-products").innerHTML = products;
+import('./bootstrap')
